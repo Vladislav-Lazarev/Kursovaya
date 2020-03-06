232ab0db51d1c4039bd189c8b020a0ca2f2449da
Shaposhnik <masyanolchik@gmail.com>
2020-03-07 01:37:57 +0200

    Added generating PDF and report activity disabled

--- app/src/main/java/com/hpcc/kursovaya/ui/report/GeneratedReportActivity.java
+++ app/src/main/java/com/hpcc/kursovaya/ui/report/GeneratedReportActivity.java
@@ -1,101 +1,157 @@
 package com.hpcc.kursovaya.ui.report;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hpcc.kursovaya.R;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GeneratedReportActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int pixels = (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics()));
+        String[] groupsName = {"П-61", "Соплежуйки", "Ветродуйки", "Моржи", "Митинг", "Вирджиния", "П-67", "П-68", "П-69", "П-70"};
         setContentView(R.layout.activity_report);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_path_150));
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //What to do on back clicked
+                finish();
+            }
+        });
+
+        String existstoragedir = getExternalFilesDir(null).getAbsolutePath() + "/report.pdf";
+        File file = new File(existstoragedir);
+
+
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(file.getAbsoluteFile()));
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        document.open();
+
+        document.setPageSize(PageSize.A4);
+        PdfPTable table = new PdfPTable(groupsName.length + 1);
+        float[] relWidths = new float[groupsName.length + 1];
+        relWidths[0] = 12f;
+        for (int i = 1; i < groupsName.length + 1; i++) {
+            relWidths[i] = 2f;
+        }
+        try {
+            table.setWidths(relWidths);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        String FONT = "/assets/fonts/arial.ttf";
+        BaseFont bf = null;
+        try {
+            bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Font fontSmall = new Font(bf, 10, Font.NORMAL);
+
+        PdfPCell cellDiscipline = new PdfPCell(new Phrase(getResources().getString(R.string.tableDisciplineHeader), fontSmall));
+        cellDiscipline.setRowspan(2);
+        cellDiscipline.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellDiscipline.setVerticalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cellDiscipline);
+        PdfPCell cellGroupHeader = new PdfPCell(new Phrase(getResources().getString(R.string.tableGroupHeader), fontSmall));
+        cellGroupHeader.setColspan(groupsName.length);
+        cellGroupHeader.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cellGroupHeader.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cellGroupHeader);
+
+        for (int i = 0; i < groupsName.length; i++) {
+            PdfPCell cellGroupName = new PdfPCell(new Phrase(groupsName[i], fontSmall));
+            cellGroupName.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cellGroupName.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            table.addCell(cellGroupName);
+        }
+        String[] subjectNames = {"Інженерія програмного забезпечення", "Основи метрології", "Інструментальні засоби візуального програмування", "Людино-машинний інтерфейс", "Проектування програмного забезпечення"};
+        for(int i = 0 ;i<subjectNames.length;i++) {
+            PdfPTable innerTable = new PdfPTable(2);
+            PdfPCell subjectCell = new PdfPCell(new Phrase(subjectNames[0], fontSmall));
+            subjectCell.setRowspan(4);
+            subjectCell.setBorder(Rectangle.NO_BORDER);
+            innerTable.addCell(subjectCell);
+            innerTable.addCell(new Phrase(getResources().getString(R.string.tablePlanHeader), fontSmall));
+            innerTable.addCell(new Phrase(getResources().getString(R.string.tableFactHeader), fontSmall));
+            innerTable.addCell(new Phrase(getResources().getString(R.string.tableCanceledHeader), fontSmall));
+            innerTable.addCell(new Phrase(getResources().getString(R.string.tableRestHeader), fontSmall));
+            PdfPCell inOutPadding = new PdfPCell(innerTable);
+            inOutPadding.setPadding(0);
+            table.addCell(inOutPadding);
+            for (int j = 0; j < groupsName.length; j++) {
+                PdfPTable hoursTable = new PdfPTable(1);
+                hoursTable.addCell(new Phrase("1"/*plan hours*/, fontSmall));
+                hoursTable.addCell(new Phrase("2"/*fact hours*/, fontSmall));
+                hoursTable.addCell(new Phrase("3"/*cancelled hours*/, fontSmall));
+                hoursTable.addCell(new Phrase("4"/*Rest hours hours*/, fontSmall));
+                PdfPCell temp = new PdfPCell((hoursTable));
+                temp.setPadding(0);
+                table.addCell(temp);
+            }
+       }
+
+        try {
+            document.add(table);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        document.close();
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
         TextView textHeader = findViewById(R.id.toolbar_title);
         textHeader.setText(R.string.report);
-        TableRow tableRow = findViewById(R.id.groupHeaderRow);
-        TextView groupFirst = findViewById(R.id.group00);
-        groupFirst.setText("П-61");
-        TextView groupSecond = new TextView(this);
-        groupSecond.setText("П-62");
-        groupSecond.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupSecond.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupSecond.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupSecond.setTextSize(16);
-        groupSecond.setWidth(pixels);
-        TextView groupThird = new TextView(this);
-        groupThird.setText("П-63");
-        groupThird.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupThird.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupThird.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupThird.setTextSize(16);
-        groupThird.setWidth(pixels);
-        TextView groupFourth = new TextView(this);
-        groupFourth.setText("П-64");
-        groupFourth.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupFourth.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupFourth.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupFourth.setTextSize(16);
-        groupFourth.setWidth(pixels);
 
-        TextView groupFifth = new TextView(this);
-        groupFifth.setText("П-65");
-        groupFifth.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupFifth.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupFifth.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupFifth.setTextSize(16);
-        groupFifth.setWidth(pixels);
-        TextView groupSixth = new TextView(this);
-        groupSixth.setText("П-66");
-        groupSixth.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupSixth.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupSixth.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupSixth.setTextSize(16);
-        groupSixth.setWidth(pixels);
-        TextView groupSeventh = new TextView(this);
-        groupSeventh.setText("П-67");
-        groupSeventh.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupSeventh.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupSeventh.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupSeventh.setTextSize(16);
-        groupSeventh.setWidth(pixels);
-        TextView groupEights = new TextView(this);
-        groupEights.setText("П-68");
-        groupEights.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupEights.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupEights.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupEights.setTextSize(16);
-        groupEights.setWidth(pixels);
-        TextView groupNineth = new TextView(this);
-        groupNineth.setText("П-69");
-        groupNineth.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupNineth.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupNineth.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupNineth.setTextSize(16);
-        groupNineth.setWidth(pixels);
-        TextView groupTenth = new TextView(this);
-        groupTenth.setText("П-70");
-        groupTenth.setBackground(getResources().getDrawable(R.drawable.grid));
-        groupTenth.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        groupTenth.setTextColor(getResources().getColor(R.color.appDefaultBlack));
-        groupTenth.setTextSize(16);
-        groupTenth.setWidth(pixels);
-        tableRow.addView(groupSecond,1);
-        tableRow.addView(groupThird,2);
-        tableRow.addView(groupFourth,3);
-        tableRow.addView(groupFifth,4);
-        tableRow.addView(groupSixth,5);
-        tableRow.addView(groupSeventh,6);
-        tableRow.addView(groupEights,7);
-        tableRow.addView(groupNineth,8);
-        tableRow.addView(groupTenth,9);
+
+
     }
 }
